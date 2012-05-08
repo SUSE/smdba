28f22175224baec5e3d7ce681da3c6c68a968d0a
Bo Maryniuk <bo@suse.de>
2012-05-08 15:09:33 +0200

    Moving stuff around

---
+++ setup.py
@@ -0,0 +1,25 @@
+__author__="bo"
+__date__ ="$May 8, 2012 2:54:34 PM$"
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup (
+  name = 'SUSE Manager Database Control',
+  version = '1.0',
+  package_dir = {'': 'src'},
+  package_data={'smdba': ['scenarios/*.scn']},
+  packages = [
+      'smdba',
+  ],
+
+  author = 'bo',
+  author_email = 'bo@suse.de',
+
+#  summary = 'SUSE Manager Database Control',
+  url = '',
+  license = 'MIT',
+  long_description= 'SUSE Manager database control to operate various database backends.',
+)
