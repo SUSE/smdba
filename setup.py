8302f2a176c7d27dee67c46d126fd5846d5c971b
Dario Leidi <dleidi@suse.com>
2016-06-21 15:50:36 +0200

    Update to 1.5.3 version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except ImportError:
 
 setup (
   name = 'SUSE Manager Database Control',
-  version = '1.5.2',
+  version = '1.5.3',
   package_dir = {'': 'src'},
   package_data={'smdba': ['scenarios/*.scn']},
   packages = [
