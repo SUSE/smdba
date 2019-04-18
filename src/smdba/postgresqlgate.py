6eb61041e2335fb9eb4b2900137d9cd0449e9cbf
Bo Maryniuk <bo@suse.de>
2019-04-18 16:45:12 +0200

    Fix type comparison

--- src/smdba/postgresqlgate.py
+++ src/smdba/postgresqlgate.py
@@ -877,7 +877,7 @@ class PgSQLGate(BaseGate):
                 backup_on = os.path.exists(backup_dst)
                 break
 
-        backup_last_transaction = None
+        backup_last_transaction = 0
         if backup_dst:
             for fname in os.listdir(backup_dst):
                 mtime = os.path.getmtime(backup_dst + "/" + fname)
