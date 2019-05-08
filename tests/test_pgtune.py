c06e22818fbbbcee0f5bf5ff2e32addcd5f1821c
Bo Maryniuk <bo@suse.de>
2019-05-08 17:18:43 +0200

    Test low memory systems are not supported

--- tests/test_pgtune.py
+++ tests/test_pgtune.py
@@ -38,3 +38,19 @@ class TestPgTune:
         assert pgtune.config['constraint_exclusion'] == 'off'
         assert pgtune.config['max_connections'] == 10
         assert pgtune.config['cpu_tuple_cost'] == '0.5'
+
+    def test_estimate_low_memory(self):
+        """
+        Estimation should abort unsupported low memory systems.
+
+        :return:
+        """
+        popen = MagicMock()
+        popen().read = MagicMock(return_value="11.2")
+
+        with patch("smdba.postgresqlgate.os.popen", popen):
+            pgtune = smdba.postgresqlgate.PgTune(10)
+            pgtune.get_total_memory = MagicMock(return_value=0xfefffff)  # One byte less to 0xff00000 memory segment
+            with pytest.raises(Exception) as exc:
+                pgtune.estimate()
+        assert "This is a low memory system and is not supported!" in str(exc)
